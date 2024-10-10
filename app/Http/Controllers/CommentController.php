ea2d1e302459304fb7a5c1f0967f1118d68d3d02
aby calago <calagoaby@gmail.com>
2024-10-10 18:09:32 +0800

    feat: add comments and connect relationship using model

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Models\Idea;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller {
+  public function store(Idea $idea) {
+    Comment::create([
+      "content" => request()->get("content"),
+      "idea_id" => $idea->id,
+    ]);
+
+    return redirect()->route("ideas.show", $idea->id)->with("success", "Commented successfully");
+  }
+}
