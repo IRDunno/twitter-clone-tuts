3ab9d97e9bc33f06d34fde4fe6eb302e85011280
aby calago <calagoaby@gmail.com>
2024-10-10 17:21:29 +0800

    feat: separated components for readability

--- resources/views/terms.blade.php
+++
@@ -1,9 +0,0 @@
-@extends("layout.layout")
-
-@section("content")
-  <h1>Terms</h1>
-  <div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat maxime laborum itaque vero numquam facilis
-    culpa, quas, dolorum aliquid saepe, repellendus dolor dolore sunt quis doloribus nam quidem id reprehenderit!
-  </div>
-@endsection
