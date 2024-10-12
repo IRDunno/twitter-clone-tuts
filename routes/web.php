0d7f20129980fabe0eb871eeddbbe3d29258cb57
aby calago <calagoaby@gmail.com>
2024-10-12 19:46:34 +0800

    feat: basic auth, login and register pages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
@@ -8,6 +9,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("/", [DashboardController::class, "index"])->name("dashboard");
 
+// The name() function references the url (/url) ONLY, the method (POST, GET, etc) is not referenced
+// So if you two with the same url, the name will depend on the method you send, the default being GET
+Route::get("/register", [AuthController::class, "register"])->name("register");
+Route::post("/register", [AuthController::class, "store"]);
+Route::get("/login", [AuthController::class, "login"])->name("login");
+Route::post("/login", [AuthController::class, "authenticate"]);
+Route::post("/logout", [AuthController::class, "logout"])->name("logout");
+
 Route::get("/ideas/{idea}", [IdeaController::class, "show"])->name("ideas.show");
 Route::post("/ideas", [IdeaController::class, "store"])->name("ideas.store");
 Route::get("/ideas/{idea}/edit", [IdeaController::class, "edit"])->name("ideas.edit");
@@ -15,6 +24,7 @@ Route::put("/ideas/{idea}", [IdeaController::class, "update"])->name("ideas.upda
 Route::delete("/ideas/{idea}", [IdeaController::class, "destroy"])->name("ideas.destroy");
 
 Route::post("/ideas/{idea}/comments", [CommentController::class, "store"])->name("ideas.comments.store");
+
 
 
 
